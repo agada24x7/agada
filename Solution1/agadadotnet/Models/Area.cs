7e2d4f4442acbd0e0e6e3c18f96bb08659ddb6a3
nishayadav857 <nishu222001@gmail.com>
2024-08-05 21:28:02 +0530

    dotnetapi file

---
+++ Solution1/agadadotnet/Models/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace agadadotnet.Models;
+
+public partial class Area
+{
+    public int AId { get; set; }
+
+    public string Aname { get; set; } = null!;
+
+    public string? Pincode { get; set; }
+
+    public int? CId { get; set; }
+
+    public virtual City? CIdNavigation { get; set; }
+
+    public virtual ICollection<Shop> Shops { get; set; } = new List<Shop>();
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
---
+++ Solution1/agadadotnet/Models/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace agadadotnet.Models;
+
+public partial class Shop
+{
+    public int ShId { get; set; }
+
+    public string Shname { get; set; } = null!;
+
+    public string? RegNo { get; set; }
+
+    public string? LicenseNo { get; set; }
+
+    public int? UId { get; set; }
+
+    public int? AId { get; set; }
+
+    public string? Address { get; set; }
+
+    public virtual Area? AIdNavigation { get; set; }
+
+    public virtual User? UIdNavigation { get; set; }
+}
